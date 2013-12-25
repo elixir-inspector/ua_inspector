90b9bedf3c74f479ed230eb58976d81ab86c2653
Marc Neudert <marc.neudert@gmail.com>
2013-12-25 18:11:04 +0100

    parses and provides access to regexes

--- lib/ex_agent/regexes.ex
+++ lib/ex_agent/regexes.ex
@@ -10,13 +10,33 @@ defmodule ExAgent.Regexes do
     regexes |> hd() |> Enum.each(fn (parser) ->
       { type, regexes } = parser
 
-      parser_type = case type do
-        "device_parsers"     -> :device
-        "os_parsers"         -> :os
-        "user_agent_parsers" -> :user_agent
-      end
+      regexes = regexes |> Enum.map(fn (regex) ->
+        raw = regex |> HashDict.new()
 
-      def get(unquote(parser_type)), do: unquote(regexes)
+        HashDict.new()
+          |> HashDict.put(:regex, raw |> HashDict.get("regex", nil) |> Regex.compile!())
+          |> HashDict.put(:device_replacement, raw |> HashDict.get("device_replacement", nil))
+          |> HashDict.put(:family_replacement, raw |> HashDict.get("family_replacement", nil))
+          |> HashDict.put(:os_replacement, raw |> HashDict.get("os_replacement", nil))
+          |> HashDict.put(:os_v1_replacement, raw |> HashDict.get("os_v1_replacement", nil))
+          |> HashDict.put(:os_v2_replacement, raw |> HashDict.get("os_v2_replacement", nil))
+          |> HashDict.put(:v1_replacement, raw |> HashDict.get("v1_replacement", nil))
+          |> HashDict.put(:v2_replacement, raw |> HashDict.get("v2_replacement", nil))
+      end)
+
+      case type do
+        "device_parsers" ->
+          @device_parsers regexes
+          def get(:device), do: @device_parsers
+
+        "os_parsers" ->
+          @os_parsers regexes
+          def get(:os), do: @os_parsers
+
+        "user_agent_parsers" ->
+          @user_agent_parsers regexes
+          def get(:user_agent), do: @user_agent_parsers
+      end
     end)
   end
 
