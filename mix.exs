99240d25611b30e0f8c1db9494353f8a6ba15975
Marc Neudert <marc.neudert@gmail.com>
2014-03-17 18:59:24 +0100

    updates to elixir 0.12.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAgent.Mixfile do
   def project do
     [ app:     :ex_agent,
       version: "0.0.1",
-      elixir:  "~> 0.12.3",
+      elixir:  "~> 0.12.5",
       deps:    deps ]
   end
 
