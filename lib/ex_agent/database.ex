4a0beed88b917cdf1cd60285bdd39b5aa6a90e25
Marc Neudert <marc.neudert@gmail.com>
2014-11-17 19:45:47 +0100

    ws / cs / doc

--- lib/ex_agent/database.ex
+++ lib/ex_agent/database.ex
@@ -7,31 +7,22 @@ defmodule ExAgent.Database do
 
   defmacro __using__(_opts) do
     quote do
-      @behaviour unquote(__MODULE__)
       @before_compile unquote(__MODULE__)
+
+      @behaviour unquote(__MODULE__)
     end
   end
 
   defmacro __before_compile__(_env) do
     quote do
-      @doc """
-      Initializes (sets up) the database.
-      """
-      @spec init() :: atom | :ets.tid
       def init() do
         :ets.new(@ets_table, [ :ordered_set, :protected, :named_table ])
       end
 
-      @doc """
-      Returns all database entries as a list.
-      """
-      @spec list() :: list
-      def list(), do: :ets.tab2list(@ets_table)
+      def list(),      do: :ets.tab2list(@ets_table)
+      def sources(),   do: @sources
+      def terminate(), do: :ets.delete(@ets_table)
 
-      @doc """
-      Loads a database file.
-      """
-      @spec load(String.t) :: no_return
       def load(path) do
         for file <- Dict.keys(@sources) do
           database = Path.join(path, file)
@@ -44,22 +35,6 @@ defmodule ExAgent.Database do
         end
       end
 
-      @doc """
-      Returns the database sources.
-      """
-      @spec sources() :: list
-      def sources(), do: @sources
-
-      @doc """
-      Terminates (deletes) the database.
-      """
-      @spec terminate() :: :true
-      def terminate(), do: :ets.delete(@ets_table)
-
-      @doc """
-      Traverses the database and passes each entry to the storage function.
-      """
-      @spec parse_database(list) :: :ok
       def parse_database([]), do: :ok
       def parse_database([ entry | database ]) do
         store_entry(entry)
@@ -69,6 +44,31 @@ defmodule ExAgent.Database do
   end
 
   @doc """
+  Initializes (sets up) the database.
+  """
+  defcallback init() :: atom | :ets.tid
+
+  @doc """
+  Returns all database entries as a list.
+  """
+  defcallback list() :: list
+
+  @doc """
+  Loads a database file.
+  """
+  defcallback load(String.t) :: no_return
+
+  @doc """
+  Traverses the database and passes each entry to the storage function.
+  """
+  defcallback parse_database(list) :: :ok
+
+  @doc """
+  Returns the database sources.
+  """
+  defcallback sources() :: list
+
+  @doc """
   Stores a database entry.
 
   If necessary a data conversion is made from the raw data passed
@@ -76,6 +76,11 @@ defmodule ExAgent.Database do
   querying the database.
   """
   defcallback store_entry(any) :: boolean
+
+  @doc """
+  Terminates (deletes) the database.
+  """
+  defcallback terminate() :: :true
 
   @doc """
   Parses a yaml database file and returns the contents.
