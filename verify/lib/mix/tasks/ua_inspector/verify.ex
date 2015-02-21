d345970e3473f9dd3667c5a213167dcd34fbbeaf
Marc Neudert <marc.neudert@gmail.com>
2015-02-21 19:39:18 +0100

    downloads fixtures before verification

--- verify/lib/mix/tasks/ua_inspector/verify.ex
+++ verify/lib/mix/tasks/ua_inspector/verify.ex
@@ -1,7 +1,11 @@
 defmodule Mix.Tasks.Ua_inspector.Verify do
   use Mix.Task
 
+  alias Mix.Tasks.Ua_inspector.Verify
+
   def run(_args) do
+    :ok = Verify.Fixtures.download()
+
     :ok
   end
 end
---
+++ verify/lib/mix/tasks/ua_inspector/verify/fixtures.ex
@@ -0,0 +1,36 @@
+defmodule Mix.Tasks.Ua_inspector.Verify.Fixtures do
+  @moduledoc """
+  Utility module to bundle/download verification fixtures.
+  """
+
+  @fixtures [
+    { "car_browser.yml", "https://raw.githubusercontent.com/piwik/device-detector/master/Tests/fixtures/car_browser.yml" }
+  ]
+
+  def download() do
+    Mix.shell.info "Download path: #{ download_path }"
+
+    setup()
+    download(@fixtures)
+
+    Mix.shell.info "Download complete!"
+    :ok
+  end
+
+  def download([]),                             do: :ok
+  def download([{ local, remote } | fixtures ]) do
+    target = Path.join([ download_path, local ])
+
+    Mix.shell.info ".. downloading: #{ local }"
+    File.write! target, Mix.Utils.read_path!(remote)
+
+    download(fixtures)
+  end
+
+  def download_path, do: Path.join(__DIR__, "../../../../../fixtures")
+
+  def setup() do
+    File.rm_rf! download_path
+    File.mkdir_p! download_path
+  end
+end
