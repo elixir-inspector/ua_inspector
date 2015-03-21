3dffd52535323950a71eceeb31817391e4cfbc5b
Marc Neudert <marc.neudert@gmail.com>
2015-03-21 18:28:45 +0100

    verifies mediaplayers

--- verify/lib/mix/tasks/ua_inspector/verify/fixtures.ex
+++ verify/lib/mix/tasks/ua_inspector/verify/fixtures.ex
@@ -11,6 +11,7 @@ defmodule Mix.Tasks.UAInspector.Verify.Fixtures do
     "console.yml",
     "desktop.yml",
     "feature_phone.yml",
+    "mediaplayer.yml",
     "phablet.yml",
     "portable_media_player.yml",
     "smart_display.yml",
