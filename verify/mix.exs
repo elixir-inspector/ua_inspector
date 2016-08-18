83c88659d04b0e9d924be28420f50bea5eb21d7d
Marc Neudert <marc.neudert@gmail.com>
2016-08-18 18:51:02 +0200

    updates verification elixir requirement

--- verify/mix.exs
+++ verify/mix.exs
@@ -4,7 +4,7 @@ defmodule UAInspector.Verification.Mixfile do
   def project do
     [ app:       :ua_inspector_verification,
       version:   "0.0.1",
-      elixir:    "~> 1.0",
+      elixir:    "~> 1.2",
       deps:      deps,
       deps_path: "../deps",
       lockfile:  "../mix.lock" ]
