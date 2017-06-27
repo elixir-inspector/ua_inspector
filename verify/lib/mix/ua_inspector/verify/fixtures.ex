de824bd87bc89d1fba7784706378513cfe3f1673
Marc Neudert <marc.neudert@gmail.com>
2017-06-27 20:53:39 +0200

    adds "smartphone-5.yml" verification fixture

--- verify/lib/mix/ua_inspector/verify/fixtures.ex
+++ verify/lib/mix/ua_inspector/verify/fixtures.ex
@@ -26,6 +26,7 @@ defmodule Mix.UAInspector.Verify.Fixtures do
     "smartphone-2.yml",
     "smartphone-3.yml",
     "smartphone-4.yml",
+    "smartphone-5.yml",
     "tablet.yml",
     "tablet-1.yml",
     "tablet-2.yml",
