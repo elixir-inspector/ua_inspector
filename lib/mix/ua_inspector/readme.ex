d18d2610ab20031c2e289881f43efe15651d462d
Marc Neudert <marc.neudert@gmail.com>
2017-11-10 18:35:20 +0100

    improves variable naming

--- lib/mix/ua_inspector/readme.ex
+++ lib/mix/ua_inspector/readme.ex
@@ -24,10 +24,10 @@ defmodule Mix.UAInspector.README do
   """
   @spec write() :: :ok
   def write() do
-    path_local = Path.dirname(path_local())
+    dirname_local = Path.dirname(path_local())
 
-    unless File.dir?(path_local) do
-      File.mkdir_p!(path_local)
+    unless File.dir?(dirname_local) do
+      File.mkdir_p!(dirname_local)
     end
 
     {:ok, _} = File.copy(path_priv(), path_local())
