bf64cc86972d625afee2bdc3ba8e4986d4d6c569
Marc Neudert <marc.neudert@gmail.com>
2018-06-16 15:38:30 +0200

    restructures parse benchmark script

--- bench/parse.exs
+++ bench/parse.exs
@@ -1,28 +1,36 @@
-agentstream =
-  [__DIR__, "agents.txt"]
-  |> Path.join()
-  |> File.read!()
-  |> Code.eval_string([], file: "agents.txt")
-  |> case do
-    {agentlist, _} when is_list(agentlist) -> agentlist
-    _ -> []
+defmodule UAInspector.Benchmark.Parse do
+  def run() do
+    [__DIR__, "agents.txt"]
+    |> Path.join()
+    |> File.read!()
+    |> Code.eval_string([], file: "agents.txt")
+    |> case do
+      {agentlist, _} when is_list(agentlist) -> agentlist
+      _ -> []
+    end
+    |> Stream.cycle()
+    |> run_benchmark()
   end
-  |> Stream.cycle()
 
-Enum.each([2, 4, 8, 16, 32], fn parallel ->
-  IO.puts("Starting parallel: #{parallel}")
+  defp run_benchmark(agentstream) do
+    Enum.each([2, 4, 8, 16, 32], fn parallel ->
+      IO.puts("Starting parallel: #{parallel}")
 
-  {us, _} =
-    :timer.tc(fn ->
-      for _ <- 1..parallel do
-        Task.async(fn ->
-          agentstream
-          |> Enum.take(1000)
-          |> Enum.each(&UAInspector.parse/1)
+      {us, _} =
+        :timer.tc(fn ->
+          for _ <- 1..parallel do
+            Task.async(fn ->
+              agentstream
+              |> Enum.take(1000)
+              |> Enum.each(&UAInspector.parse/1)
+            end)
+          end
+          |> Enum.map(&Task.await(&1, :infinity))
         end)
-      end
-      |> Enum.map(&Task.await(&1, :infinity))
-    end)
 
-  IO.puts("done in #{us} microseconds")
-end)
+      IO.puts("done in #{us} microseconds")
+    end)
+  end
+end
+
+UAInspector.Benchmark.Parse.run()
