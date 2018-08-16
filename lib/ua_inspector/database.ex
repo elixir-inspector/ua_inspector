7d221c177e1326eb70563f5c21a9390ab8d5c912
Marc Neudert <marc.neudert@gmail.com>
2018-08-16 20:50:53 +0200

    deduplicates storage server logic

--- lib/ua_inspector/database.ex
+++ lib/ua_inspector/database.ex
@@ -14,47 +14,6 @@ defmodule UAInspector.Database do
       alias UAInspector.Storage.State
       alias UAInspector.Util.YAML
 
-      @behaviour unquote(__MODULE__)
-
-      @ets_cleanup_delay_default 30_000
-
-      # GenServer lifecycle
-
-      def init(_) do
-        :ok = GenServer.cast(__MODULE__, :reload)
-
-        {:ok, %State{}}
-      end
-
-      # GenServer callbacks
-
-      def handle_call(:ets_tid, _from, state) do
-        {:reply, state.ets_tid, state}
-      end
-
-      def handle_cast(:reload, %State{ets_tid: old_ets_tid}) do
-        state = %State{ets_tid: ETS.create(__MODULE__)}
-
-        :ok = load(state.ets_tid)
-        :ok = schedule_ets_cleanup(old_ets_tid)
-
-        {:noreply, state}
-      end
-
-      def handle_info({:delete_ets_table, nil}, state), do: {:noreply, state}
-
-      def handle_info({:delete_ets_table, ets_tid}, state) do
-        case state.ets_tid == ets_tid do
-          true ->
-            # ignore call!
-            {:noreply, state}
-
-          false ->
-            :ok = ETS.delete(ets_tid)
-            {:noreply, state}
-        end
-      end
-
       # Public methods
 
       def sources do
@@ -97,16 +56,6 @@ defmodule UAInspector.Database do
       end
 
       defp parse_database([], _, _ets_tid, index), do: index
-
-      defp schedule_ets_cleanup(ets_tid) do
-        Process.send_after(
-          self(),
-          {:delete_ets_table, ets_tid},
-          Config.get(:ets_cleanup_delay, @ets_cleanup_delay_default)
-        )
-
-        :ok
-      end
     end
   end
 
--- lib/ua_inspector/short_code_map.ex
+++ lib/ua_inspector/short_code_map.ex
@@ -10,50 +10,9 @@ defmodule UAInspector.ShortCodeMap do
       require Logger
 
       alias UAInspector.Config
-      alias UAInspector.Storage.ETS
-      alias UAInspector.Storage.State
       alias UAInspector.Util.YAML
 
       @behaviour unquote(__MODULE__)
-
-      @ets_cleanup_delay_default 30_000
-
-      # GenServer lifecycle
-
-      def init(_) do
-        :ok = GenServer.cast(__MODULE__, :reload)
-
-        {:ok, %State{}}
-      end
-
-      # GenServer callbacks
-
-      def handle_call(:ets_tid, _from, state) do
-        {:reply, state.ets_tid, state}
-      end
-
-      def handle_cast(:reload, %State{ets_tid: old_ets_tid} = state) do
-        state = %State{ets_tid: ETS.create(__MODULE__)}
-
-        :ok = load(state.ets_tid)
-        :ok = schedule_ets_cleanup(old_ets_tid)
-
-        {:noreply, state}
-      end
-
-      def handle_info({:delete_ets_table, nil}, state), do: {:noreply, state}
-
-      def handle_info({:delete_ets_table, ets_tid}, state) do
-        case state.ets_tid == ets_tid do
-          true ->
-            # ignore call!
-            {:noreply, state}
-
-          false ->
-            :ok = ETS.delete(ets_tid)
-            {:noreply, state}
-        end
-      end
 
       # Public methods
 
@@ -99,16 +58,6 @@ defmodule UAInspector.ShortCodeMap do
       end
 
       defp parse_map([], _ets_tid), do: :ok
-
-      defp schedule_ets_cleanup(ets_tid) do
-        Process.send_after(
-          self(),
-          {:delete_ets_table, ets_tid},
-          Config.get(:ets_cleanup_delay, @ets_cleanup_delay_default)
-        )
-
-        :ok
-      end
     end
   end
 
