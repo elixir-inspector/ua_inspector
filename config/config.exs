463561a853cf82f541d92086cbf54f34c3fcc8d0
Marc Neudert <marc.neudert@gmail.com>
2018-12-08 12:08:53 +0100

    improves path configurations

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,12 @@
 use Mix.Config
 
 if Mix.env() == :bench do
-  config :ua_inspector, database_path: Path.join(__DIR__, "../data")
+  config :ua_inspector, database_path: Path.expand("../data", __DIR__)
 end
 
 if Mix.env() == :test do
   config :ua_inspector,
-    database_path: Path.join(__DIR__, "../test/fixtures"),
+    database_path: Path.expand("../test/fixtures", __DIR__),
     ets_cleanup_delay: 10,
     pool: [max_overflow: 0, size: 1]
 end
--- verify/config/config.exs
+++ verify/config/config.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :ua_inspector,
-  database_path: Path.join(__DIR__, "../databases"),
+  database_path: Path.expand("../databases", __DIR__),
   pool: [max_overflow: 5, size: 10]
