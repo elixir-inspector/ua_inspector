bfcfec2f2f942a6008627566cd31910e0be28f6e
Marc Neudert <marc.neudert@gmail.com>
2018-12-29 13:25:59 +0100

    adds "smartphone-7.yml" verification fixture

--- verify/lib/mix/ua_inspector/verify/fixtures.ex
+++ verify/lib/mix/ua_inspector/verify/fixtures.ex
@@ -27,6 +27,7 @@ defmodule Mix.UAInspector.Verify.Fixtures do
     "smartphone-4.yml",
     "smartphone-5.yml",
     "smartphone-6.yml",
+    "smartphone-7.yml",
     "tablet.yml",
     "tablet-1.yml",
     "tablet-2.yml",
