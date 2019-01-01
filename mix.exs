f53a9cd5bcaebcfb2f0284a659ea0beae9934be1
Marc Neudert <marc.neudert@gmail.com>
2019-01-01 13:08:03 +0100

    runs credo on travis

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule UAInspector.Mixfile do
   defp deps do
     [
       {:benchee, "~> 0.11.0", only: :bench, runtime: false},
+      {:credo, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.9", only: :test, runtime: false},
       {:hackney, "~> 1.0"},
