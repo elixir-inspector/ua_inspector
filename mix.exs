66872aec78148cf6ac1ef5438a2ec2712ac01f98
Marc Neudert <marc.neudert@gmail.com>
2019-01-01 13:12:54 +0100

    adds :dialyxir dependency

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule UAInspector.Mixfile do
     [
       {:benchee, "~> 0.11.0", only: :bench, runtime: false},
       {:credo, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.9", only: :test, runtime: false},
       {:hackney, "~> 1.0"},
