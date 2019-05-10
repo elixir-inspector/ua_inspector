d6496674a2b82940e5a1f872c09117852eb75236
Marc Neudert <marc.neudert@gmail.com>
2019-05-10 17:39:09 +0200

    Improve internal parser pattern matching

--- lib/ua_inspector/parser.ex
+++ lib/ua_inspector/parser.ex
@@ -71,12 +71,14 @@ defmodule UAInspector.Parser do
     }
   end
 
-  defp maybe_detect_desktop(%{device: %{type: :unknown}} = result) do
-    desktop_only = Util.OS.desktop_only?(result.os)
-    mobile_only = Util.Client.mobile_only?(result.client)
+  defp maybe_detect_desktop(
+         %{client: client, device: %{type: :unknown} = device, os: os} = result
+       ) do
+    desktop_only = Util.OS.desktop_only?(os)
+    mobile_only = Util.Client.mobile_only?(client)
 
     case desktop_only && !mobile_only do
-      true -> %{result | device: %{result.device | type: "desktop"}}
+      true -> %{result | device: %{device | type: "desktop"}}
       false -> result
     end
   end
@@ -84,14 +86,14 @@ defmodule UAInspector.Parser do
   defp maybe_detect_desktop(result), do: result
 
   # assume some browsers to be a tv
-  defp maybe_detect_tv(%{client: %{name: "Kylo"}, device: %{type: :unknown}} = result) do
-    %{result | device: %{result.device | type: "tv"}}
+  defp maybe_detect_tv(%{client: %{name: "Kylo"}, device: %{type: :unknown} = device} = result) do
+    %{result | device: %{device | type: "tv"}}
   end
 
   defp maybe_detect_tv(
-         %{client: %{name: "Espial TV Browser"}, device: %{type: :unknown}} = result
+         %{client: %{name: "Espial TV Browser"}, device: %{type: :unknown} = device} = result
        ) do
-    %{result | device: %{result.device | type: "tv"}}
+    %{result | device: %{device | type: "tv"}}
   end
 
   defp maybe_detect_tv(result), do: result
@@ -103,41 +105,46 @@ defmodule UAInspector.Parser do
     result
   end
 
-  defp maybe_fix_android(%{os: %{name: os_name}, device: %{type: "feature phone"}} = result) do
+  defp maybe_fix_android(
+         %{device: %{type: "feature phone"} = device, os: %{name: os_name}} = result
+       ) do
     short_code = ShortCodeMap.OSs.to_short(os_name)
     family = Util.OS.family(short_code)
 
     case family do
-      "Android" -> %{result | device: %{result.device | type: "smartphone"}}
+      "Android" -> %{result | device: %{device | type: "smartphone"}}
       _ -> result
     end
   end
 
-  defp maybe_fix_android(%{os: %{name: "Android"} = os, device: %{type: :unknown}} = result) do
-    version = Util.to_semver(os.version)
+  defp maybe_fix_android(
+         %{device: %{type: :unknown} = device, os: %{name: "Android", version: os_version}} =
+           result
+       ) do
+    version = Util.to_semver(os_version)
 
     type =
       cond do
         smartphone_android?(version) -> "smartphone"
         tablet_android?(version) -> "tablet"
-        true -> result.device.type
+        true -> :unknown
       end
 
-    %{result | device: %{result.device | type: type}}
+    %{result | device: %{device | type: type}}
   end
 
   defp maybe_fix_android(result), do: result
 
-  defp maybe_fix_ios(%{device: %{brand: :unknown}, os: %{name: "Apple TV"}} = result) do
-    %{result | device: %{result.device | brand: "Apple"}}
+  defp maybe_fix_ios(%{device: %{brand: :unknown} = device, os: %{name: "Apple TV"}} = result) do
+    %{result | device: %{device | brand: "Apple"}}
   end
 
-  defp maybe_fix_ios(%{device: %{brand: :unknown}, os: %{name: "iOS"}} = result) do
-    %{result | device: %{result.device | brand: "Apple"}}
+  defp maybe_fix_ios(%{device: %{brand: :unknown} = device, os: %{name: "iOS"}} = result) do
+    %{result | device: %{device | brand: "Apple"}}
   end
 
-  defp maybe_fix_ios(%{device: %{brand: :unknown}, os: %{name: "Mac"}} = result) do
-    %{result | device: %{result.device | brand: "Apple"}}
+  defp maybe_fix_ios(%{device: %{brand: :unknown} = device, os: %{name: "Mac"}} = result) do
+    %{result | device: %{device | brand: "Apple"}}
   end
 
   defp maybe_fix_ios(result), do: result
@@ -167,22 +174,22 @@ defmodule UAInspector.Parser do
 
   @is_chrome_smartphone Util.build_regex("Chrome/[\.0-9]* Mobile")
 
-  defp fix_android_chrome(result) do
+  defp fix_android_chrome(%{device: device, user_agent: ua} = result) do
     type =
-      case Regex.match?(@is_chrome_smartphone, result.user_agent) do
+      case Regex.match?(@is_chrome_smartphone, ua) do
         true -> "smartphone"
         false -> "tablet"
       end
 
-    %{result | device: %{result.device | type: type}}
+    %{result | device: %{device | type: type}}
   end
 
   # assume "Opera TV Store" to be a tv
   @is_opera_tv_store Util.build_regex("Opera TV Store")
 
-  defp maybe_fix_opera_tv_store(%{device: %{type: :unknown}} = result) do
-    if Regex.match?(@is_opera_tv_store, result.user_agent) do
-      %{result | device: %{result.device | type: "tv"}}
+  defp maybe_fix_opera_tv_store(%{device: %{type: :unknown} = device, user_agent: ua} = result) do
+    if Regex.match?(@is_opera_tv_store, ua) do
+      %{result | device: %{device | type: "tv"}}
     else
       result
     end
@@ -193,21 +200,29 @@ defmodule UAInspector.Parser do
   # assume windows 8 with touch capability is a tablet
   @has_touch Util.build_regex("Touch")
 
-  defp maybe_fix_windows(%{os: %{name: "Windows RT"}, device: %{type: :unknown}} = result) do
-    %{result | device: %{result.device | type: "tablet"}}
+  defp maybe_fix_windows(
+         %{device: %{type: :unknown} = device, os: %{name: "Windows RT"}} = result
+       ) do
+    %{result | device: %{device | type: "tablet"}}
   end
 
   defp maybe_fix_windows(%{os: %{version: :unknown}} = result) do
     result
   end
 
-  defp maybe_fix_windows(%{os: %{name: "Windows"} = os, device: %{type: :unknown}} = result) do
-    version = Util.to_semver(os.version)
+  defp maybe_fix_windows(
+         %{
+           device: %{type: :unknown} = device,
+           os: %{name: "Windows", version: os_version},
+           user_agent: ua
+         } = result
+       ) do
+    version = Util.to_semver(os_version)
     is_gte_8 = :lt != Version.compare(version, "8.0.0")
-    is_touch = Regex.match?(@has_touch, result.user_agent)
+    is_touch = Regex.match?(@has_touch, ua)
 
     if is_gte_8 && is_touch do
-      %{result | device: %{result.device | type: "tablet"}}
+      %{result | device: %{device | type: "tablet"}}
     else
       result
     end
