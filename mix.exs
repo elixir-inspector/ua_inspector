1fd394496d589328048f340f2eb6de184e68206f
Marc Neudert <marc.neudert@gmail.com>
2019-07-13 15:15:55 +0200

    Bump version to v1.2.0-dev

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule UAInspector.MixProject do
     [
       app: :ua_inspector,
       name: "UAInspector",
-      version: "1.1.0",
+      version: "1.2.0-dev",
       elixir: "~> 1.5",
       aliases: aliases(),
       deps: deps(),
@@ -68,7 +68,7 @@ defmodule UAInspector.MixProject do
   defp docs do
     [
       main: "UAInspector",
-      source_ref: "v1.1.0",
+      source_ref: "master",
       source_url: @url_github
     ]
   end
