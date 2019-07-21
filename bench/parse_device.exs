12e034b1718f9ee94ffa5170f32ca81ab97621cb
Marc Neudert <marc.neudert@gmail.com>
2019-07-21 15:06:49 +0200

    Please the linting gods

--- bench/parse_device.exs
+++ bench/parse_device.exs
@@ -1,4 +1,6 @@
 defmodule UAInspector.Benchmark.ParseDevice do
+  alias UAInspector.Parser.Device
+
   @agent_desktop "Mozilla/5.0 (AmigaOS; U; AmigaOS 1.3; en-US; rv:1.8.1.21) Gecko/20090303 SeaMonkey/1.1.15"
   @agent_smartphone "Mozilla/5.0 (Linux; Android 6.0; U007 Pro Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36"
   @agent_tablet "Mozilla/5.0 (Linux; U; Android 4.2.2; it-it; Surfing TAB B 9.7 3G Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
@@ -7,10 +9,10 @@ defmodule UAInspector.Benchmark.ParseDevice do
   def run do
     Benchee.run(
       %{
-        "Parse Device: desktop" => fn -> UAInspector.Parser.Device.parse(@agent_desktop) end,
-        "Parse Device: smartphone" => fn -> UAInspector.Parser.Device.parse(@agent_smartphone) end,
-        "Parse Device: tablet" => fn -> UAInspector.Parser.Device.parse(@agent_tablet) end,
-        "Parse Device: unknown" => fn -> UAInspector.Parser.Device.parse(@agent_unknown) end
+        "Parse Device: desktop" => fn -> Device.parse(@agent_desktop) end,
+        "Parse Device: smartphone" => fn -> Device.parse(@agent_smartphone) end,
+        "Parse Device: tablet" => fn -> Device.parse(@agent_tablet) end,
+        "Parse Device: unknown" => fn -> Device.parse(@agent_unknown) end
       },
       formatters: [{Benchee.Formatters.Console, comparison: false}],
       warmup: 2,
--- bench/parse_os.exs
+++ bench/parse_os.exs
@@ -1,4 +1,6 @@
 defmodule UAInspector.Benchmark.ParseOS do
+  alias UAInspector.Parser.OS
+
   @agent_desktop "Mozilla/5.0 (AmigaOS; U; AmigaOS 1.3; en-US; rv:1.8.1.21) Gecko/20090303 SeaMonkey/1.1.15"
   @agent_smartphone "Mozilla/5.0 (Linux; Android 6.0; U007 Pro Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36"
   @agent_tablet "Mozilla/5.0 (Linux; U; Android 4.2.2; it-it; Surfing TAB B 9.7 3G Build/JDQ39) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
@@ -7,10 +9,10 @@ defmodule UAInspector.Benchmark.ParseOS do
   def run do
     Benchee.run(
       %{
-        "Parse OS: desktop" => fn -> UAInspector.Parser.OS.parse(@agent_desktop) end,
-        "Parse OS: smartphone" => fn -> UAInspector.Parser.OS.parse(@agent_smartphone) end,
-        "Parse OS: tablet" => fn -> UAInspector.Parser.OS.parse(@agent_tablet) end,
-        "Parse OS: unknown" => fn -> UAInspector.Parser.OS.parse(@agent_unknown) end
+        "Parse OS: desktop" => fn -> OS.parse(@agent_desktop) end,
+        "Parse OS: smartphone" => fn -> OS.parse(@agent_smartphone) end,
+        "Parse OS: tablet" => fn -> OS.parse(@agent_tablet) end,
+        "Parse OS: unknown" => fn -> OS.parse(@agent_unknown) end
       },
       formatters: [{Benchee.Formatters.Console, comparison: false}],
       warmup: 2,
