71c244eb116b539db3b3063ec136e04d6cc01db3
Marc Neudert <marc.neudert@gmail.com>
2020-10-27 19:40:33 +0100

    Add new verification fixtures

--- verify/lib/mix/ua_inspector/verify/fixtures.ex
+++ verify/lib/mix/ua_inspector/verify/fixtures.ex
@@ -38,11 +38,14 @@ defmodule Mix.UAInspector.Verify.Fixtures do
     "smartphone-14.yml",
     "smartphone-15.yml",
     "smartphone-16.yml",
+    "smartphone-17.yml",
+    "smartphone-18.yml",
     "tablet.yml",
     "tablet-1.yml",
     "tablet-2.yml",
     "tablet-3.yml",
     "tablet-4.yml",
+    "tablet-5.yml",
     "tv.yml",
     "unknown.yml",
     "wearable.yml"
@@ -72,6 +75,20 @@ defmodule Mix.UAInspector.Verify.Fixtures do
 
   defp download_fixture(remote, local) do
     {:ok, content} = Config.downloader_adapter().read_remote(remote)
+
+    content =
+      case Path.basename(local) do
+        "smartphone-17.yml" ->
+          # Fix triple whitespace breaking :yamerl
+          String.replace(
+            content,
+            "   user_agent: Mozilla/5.0 (Linux; U; Android 10; en-us; RMX1973 Build/QKQ1.190918.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 HeyTapBrowser/45.7.1.9",
+            "  user_agent: Mozilla/5.0 (Linux; U; Android 10; en-us; RMX1973 Build/QKQ1.190918.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 HeyTapBrowser/45.7.1.9"
+          )
+
+        _ ->
+          content
+      end
 
     File.write!(local, content)
   end
