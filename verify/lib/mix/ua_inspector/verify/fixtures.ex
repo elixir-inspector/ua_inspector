c5209218baef55fc989dd7dab6906a38e855054c
Marc Neudert <marc.neudert@gmail.com>
2021-11-25 18:42:20 +0100

    Add "smartphone-28.yml" verification fixture

--- verify/lib/mix/ua_inspector/verify/fixtures.ex
+++ verify/lib/mix/ua_inspector/verify/fixtures.ex
@@ -50,6 +50,7 @@ defmodule Mix.UAInspector.Verify.Fixtures do
     "smartphone-25.yml",
     "smartphone-26.yml",
     "smartphone-27.yml",
+    "smartphone-28.yml",
     "tablet.yml",
     "tablet-1.yml",
     "tablet-2.yml",
