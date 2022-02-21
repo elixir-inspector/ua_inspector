c71164bdbc966b7ff3dacfe4685f0f6fbaac31b7
Marc Neudert <marc.neudert@gmail.com>
2022-02-21 19:26:09 +0100

    Remove not longer used ShortCodeMap.to_long/2

--- lib/ua_inspector/util/short_code_map.ex
+++ lib/ua_inspector/util/short_code_map.ex
@@ -2,14 +2,6 @@ defmodule UAInspector.Util.ShortCodeMap do
   @moduledoc false
 
   @doc """
-  Extracts the long version for a short code.
-  """
-  @spec to_long([{String.t(), String.t()}], String.t()) :: String.t()
-  def to_long([], short), do: short
-  def to_long([{short, long} | _], short), do: long
-  def to_long([_ | rest], short), do: to_long(rest, short)
-
-  @doc """
   Extracts the short version for an expanded short code.
   """
   @spec to_short([{String.t(), String.t()}], String.t()) :: String.t()
