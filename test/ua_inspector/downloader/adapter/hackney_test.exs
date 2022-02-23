bc752b4d586e653b27bef36bee3209191ee8cc8b
Marc Neudert <marc.neudert@gmail.com>
2022-02-23 19:39:40 +0100

    Ensure non 200 responses in download adapter return error

--- test/ua_inspector/downloader/adapter/hackney_test.exs
+++ test/ua_inspector/downloader/adapter/hackney_test.exs
@@ -6,4 +6,23 @@ defmodule UAInspector.Downloader.Adapter.HackneyTest do
   test "errors returned from adapter" do
     assert {:error, :nxdomain} = Hackney.read_remote("invalid")
   end
+
+  test "requires HTTP 200 responses for success" do
+    Application.ensure_all_started(:inets)
+
+    httpd_opts = [
+      port: 0,
+      server_name: 'ua_inspector_hackney_test',
+      server_root: String.to_charlist(__DIR__),
+      document_root: String.to_charlist(__DIR__)
+    ]
+
+    {:ok, httpd_pid} = :inets.start(:httpd, httpd_opts)
+
+    location = "http://localhost:#{:httpd.info(httpd_pid)[:port]}/--does-not-exist--"
+
+    assert {:error, {:status, 404, ^location}} = Hackney.read_remote(location)
+
+    :inets.stop(:httpd, httpd_pid)
+  end
 end
