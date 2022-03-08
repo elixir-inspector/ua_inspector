091ce0f1576e20466d73eed6f7a7ad572b2a6a30
Marc Neudert <marc.neudert@gmail.com>
2022-03-08 19:25:47 +0100

    Bump version to v3.0.1-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule UAInspector.MixProject do
 
   @url_changelog "https://hexdocs.pm/ua_inspector/changelog.html"
   @url_github "https://github.com/elixir-inspector/ua_inspector"
-  @version "3.0.0"
+  @version "3.0.1-dev"
 
   def project do
     [
