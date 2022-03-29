10e1864b59b08d21b2e19aea89d1f4729cbb223d
Marc Neudert <marc.neudert@gmail.com>
2022-03-29 19:01:53 +0200

    Prepare verification of OS parser

---
+++ verify/lib/mix/ua_inspector/verify/cleanup_os.ex
@@ -0,0 +1,40 @@
+defmodule Mix.UAInspector.Verify.CleanupOS do
+  @moduledoc false
+
+  @empty_to_unknown [
+    [:os, :name],
+    [:os, :version],
+    [:os, :platform]
+  ]
+
+  @doc """
+  Cleans up a test case.
+  """
+  @spec cleanup(testcase :: map) :: map
+  def cleanup(testcase) do
+    testcase
+    |> convert_empty(@empty_to_unknown, :unknown)
+    |> version_to_string()
+  end
+
+  defp convert_empty(testcase, [], _), do: testcase
+
+  defp convert_empty(testcase, [path | paths], replacement) do
+    testcase
+    |> get_in(path)
+    |> case do
+      :null -> put_in(testcase, path, replacement)
+      "" -> put_in(testcase, path, replacement)
+      _ -> testcase
+    end
+    |> convert_empty(paths, replacement)
+  rescue
+    FunctionClauseError -> convert_empty(testcase, paths, replacement)
+  end
+
+  defp version_to_string(%{os: %{version: version}} = testcase) when is_integer(version) do
+    put_in(testcase, [:os, :version], to_string(version))
+  end
+
+  defp version_to_string(testcase), do: testcase
+end
