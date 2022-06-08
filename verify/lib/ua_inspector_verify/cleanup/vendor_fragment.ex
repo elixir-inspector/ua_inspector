d01a7a2aff21faeafe4000428c38e0b8f3fc6e83
Marc Neudert <marc.neudert@gmail.com>
2022-06-08 18:18:01 +0200

    Delete unused key from vendor fragment fixture after cleanup

--- verify/lib/ua_inspector_verify/cleanup/vendor_fragment.ex
+++ verify/lib/ua_inspector_verify/cleanup/vendor_fragment.ex
@@ -6,6 +6,8 @@ defmodule UAInspectorVerify.Cleanup.VendorFragment do
   """
   @spec cleanup(testcase :: map) :: map
   def cleanup(%{useragent: ua} = testcase) do
-    Map.put(testcase, :user_agent, ua)
+    testcase
+    |> Map.delete(:useragent)
+    |> Map.put(:user_agent, ua)
   end
 end
