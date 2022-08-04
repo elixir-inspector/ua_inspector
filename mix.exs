cc3fbe5e6ed34f549d928b2709ce0a4fb7e45a70
Marc Neudert <marc.neudert@gmail.com>
2022-08-04 17:09:34 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -47,9 +47,9 @@ defmodule UAInspector.MixProject do
 
   defp deps do
     [
-      {:benchee, "~> 1.0", only: :bench, runtime: false},
-      {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
+      {:benchee, "~> 1.1", only: :bench, runtime: false},
+      {:credo, "~> 1.6", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
       {:hackney, "~> 1.0"},
