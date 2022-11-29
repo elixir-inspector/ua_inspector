2c08cae26181b6c13af5d54ee0e7e216a605dde6
Marc Neudert <marc.neudert@gmail.com>
2022-11-29 17:58:52 +0100

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule UAInspector.MixProject do
       {:credo, "~> 1.6", only: :dev, runtime: false},
       {:dialyxir, "~> 1.2", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
+      {:excoveralls, "~> 0.15.0", only: :test, runtime: false},
       {:hackney, "~> 1.0"},
       {:yamerl, "~> 0.7"}
     ]
