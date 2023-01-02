3149c2032d693be11b309c0922db07725fe9bb3f
Marc Neudert <marc.neudert@gmail.com>
2023-01-02 12:00:43 +0100

    Add explicit tests for OS utility

--- test/ua_inspector/util/os_test.exs
+++ test/ua_inspector/util/os_test.exs
@@ -1,5 +1,27 @@
 defmodule UAInspector.Util.OSTest do
   use ExUnit.Case, async: true
 
+  alias UAInspector.Result
+  alias UAInspector.Util.OS
+
   doctest UAInspector.Util.OS, import: true
+
+  test "check desktop only" do
+    assert OS.desktop_only?("Debian")
+
+    refute OS.desktop_only?("Android")
+  end
+
+  test "short code family lookup" do
+    assert "Firefox OS" = OS.family("FOS")
+
+    refute OS.family("XXX")
+  end
+
+  test "result family lookup" do
+    assert "GNU/Linux" = OS.family_from_result(%Result.OS{name: "Debian"})
+
+    assert :unknown = OS.family_from_result(%Result.OS{name: "Unknown OS"})
+    assert :unknown = OS.family_from_result(%Result.OS{name: :unknown})
+  end
 end
