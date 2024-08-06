da5f7344dd73904b0ee5cdcafdb8a086dd9df965
Marc Neudert <marc.neudert@gmail.com>
2024-08-06 18:32:49 +0200

    Please the formatting gods

--- test/ua_inspector/util/version_test.exs
+++ test/ua_inspector/util/version_test.exs
@@ -12,11 +12,12 @@ defmodule UAInspector.Util.VersionTest do
     for spec <- specs do
       [v1, op, v2] = spec |> String.trim() |> String.split(" ")
 
-      result = case op do
-        "<" -> :lt
-        ">" -> :gt
-        "==" -> :eq
-      end
+      result =
+        case op do
+          "<" -> :lt
+          ">" -> :gt
+          "==" -> :eq
+        end
 
       assert ^result = Version.compare_canonicalized(v1, v2)
     end
