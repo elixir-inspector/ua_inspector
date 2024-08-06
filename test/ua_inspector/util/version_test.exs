385f502284df6cff86ffe0cf071e72be5f6fdb1a
Marc Neudert <marc.neudert@gmail.com>
2024-08-06 15:12:39 +0200

    Define fixture with canonicalized version comparison expectations

--- test/ua_inspector/util/version_test.exs
+++ test/ua_inspector/util/version_test.exs
@@ -1,5 +1,24 @@
 defmodule UAInspector.Util.VersionTest do
   use ExUnit.Case, async: true
 
-  doctest UAInspector.Util.Version, import: true
+  alias UAInspector.Util.Version
+
+  doctest Version, import: true
+
+  test "compare canonicalized" do
+    source = Path.expand("../../fixtures/versions.txt", __DIR__)
+    specs = File.stream!(source)
+
+    for spec <- specs do
+      [v1, op, v2] = spec |> String.trim() |> String.split(" ")
+
+      result = case op do
+        "<" -> :lt
+        ">" -> :gt
+        "==" -> :eq
+      end
+
+      assert ^result = Version.compare_canonicalized(v1, v2)
+    end
+  end
 end
