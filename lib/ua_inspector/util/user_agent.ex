4cae17181bb72cd6b2071fb14a12b0ea8212d6c7
Marc Neudert <marc.neudert@gmail.com>
2025-07-20 10:56:07 +0200

    Inline user agent utility regular expressions

--- lib/ua_inspector/util/user_agent.ex
+++ lib/ua_inspector/util/user_agent.ex
@@ -4,28 +4,27 @@ defmodule UAInspector.Util.UserAgent do
   alias UAInspector.ClientHints
   alias UAInspector.Util
 
-  @client_hints_fragment Regex.compile!(
-                           "Android (?:10[.\d]*; K(?: Build/|[;)])|1[1-5]\\)) AppleWebKit",
-                           [:caseless]
-                         )
-
-  @desktop_pos Util.Regex.build_regex("(?:Windows (?:NT|IoT)|X11; Linux x86_64)")
-  @desktop_neg Util.Regex.build_regex(
-                 "CE-HTML| Mozilla/|Andr[o0]id|Tablet|Mobile|iPhone|Windows Phone|ricoh|OculusBrowser|PicoBrowser|Lenovo|compatible; MSIE|Trident/|Tesla/|XBOX|FBMD/|ARM; ?([^)]+)"
-               )
-
   @doc """
   Returns if the parsed UA contains the 'Windows NT;' or 'X11; Linux x86_64' fragments
   """
   @spec has_desktop_fragment?(String.t()) :: boolean
-  def has_desktop_fragment?(ua),
-    do: Regex.match?(@desktop_pos, ua) && !Regex.match?(@desktop_neg, ua)
+  def has_desktop_fragment?(ua) do
+    re_desktop_pos = Util.Regex.build_regex("(?:Windows (?:NT|IoT)|X11; Linux x86_64)")
+
+    re_desktop_neg =
+      Util.Regex.build_regex(
+        "CE-HTML| Mozilla/|Andr[o0]id|Tablet|Mobile|iPhone|Windows Phone|ricoh|OculusBrowser|PicoBrowser|Lenovo|compatible; MSIE|Trident/|Tesla/|XBOX|FBMD/|ARM; ?([^)]+)"
+      )
+
+    Regex.match?(re_desktop_pos, ua) && !Regex.match?(re_desktop_neg, ua)
+  end
 
   @doc """
   Returns if the parsed UA contains the 'Android 10 K;' or Android 10 K Build/` fragment
   """
   @spec has_client_hints_fragment?(String.t()) :: boolean
-  def has_client_hints_fragment?(ua), do: Regex.match?(@client_hints_fragment, ua)
+  def has_client_hints_fragment?(ua),
+    do: Regex.match?(~r/Android (?:10[.\d]*; K(?: Build\/|[;)])|1[1-5]\)) AppleWebKit/i, ua)
 
   @doc """
   Patch "frozen" user agents for improved detection.
